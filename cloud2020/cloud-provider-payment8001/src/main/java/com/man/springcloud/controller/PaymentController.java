206cc9afb852f4e540fe58d17d6971e5b28661f2
mark <mark@123.com>
2021-04-02 00:04:54 +0800

    RequestBody很重要

--- cloud2020/cloud-provider-payment8001/src/main/java/com/man/springcloud/controller/PaymentController.java
+++ cloud2020/cloud-provider-payment8001/src/main/java/com/man/springcloud/controller/PaymentController.java
@@ -4,10 +4,7 @@ import com.man.springcloud.entities.CommonResult;
 import com.man.springcloud.entities.Payment;
 import com.man.springcloud.service.impl.PaymentServiceImpl;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -19,7 +16,7 @@ public class PaymentController {
     private PaymentServiceImpl paymentService;
 
     @PostMapping("/payment/create")
-    public CommonResult create(Payment payment){
+    public CommonResult create(@RequestBody Payment payment){
         int result = paymentService.create(payment);
         log.info("*********插入结果:"+result);
         if (result>0){
